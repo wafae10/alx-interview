c1b2011d0acdde37c1f362227f81cb4ba03e1ae8
wafae10 <zwafae13@gmail.com>
2024-05-09 03:13:09 -0700

    up

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module that contains the island_perimeter  """
+
+
+def island_perimeter(grid):
+    """ 
+    Returns:
+        perimeter (int): Perimeter of the island.
+
+    """
+    perimeter = 0
+
+    if grid != []:
+        nColumns = len(grid[0])
+        nRows = len(grid)
+
+    for a in range(nRows):
+        for b in range(nColumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nRows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == nColumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+new interview project
